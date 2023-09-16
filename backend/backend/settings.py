d806a5afb5eb54befa74cae1e9961589646f932a
Александра Никитенко <aleksandri@cn.ru>
2023-09-16 18:53:27 +0700

    fix settings13

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,13 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1 localhost').split()
