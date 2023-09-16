53da6a40031cc2dfa28499038aaf157c6fc55893
Александра Никитенко <aleksandri@cn.ru>
2023-09-16 17:50:22 +0700

    fix settings9

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,9 +16,9 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'https://foodgram.ddnsking.com').split()
-
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1 localhost').split()
+
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'https://foodgram.ddnsking.com').split()
 
 LOGGING = {
     'version': 1,
