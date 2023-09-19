5e9fe7e78cc2bdbbace9d5d5e8689ef244a890a6
Александра Никитенко <aleksandri@cn.ru>
2023-09-19 19:55:28 +0700

    users permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -16,3 +16,22 @@ class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
             obj.author == request.user
             or request.user.is_superuser
         )
+
+
+class IsAuthenticatedOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if view.action == 'me' and not request.user.is_authenticated:
+            return False
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
+    
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return (
+            obj.author == request.user
+            or request.user.is_superuser
+        )
+    
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -103,20 +103,20 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#     },
-# ]
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 
 # Internationalization
@@ -160,8 +160,7 @@ DJOSER = {
     'HIDE_USERS': False,
     'PERMISSIONS': {
         'user_list': ['rest_framework.permissions.IsAuthenticatedOrReadOnly'],
-        'current_user': ['djoser.permissions.CurrentUserOrAdminOrReadOnly'],
-        'user': ['rest_framework.permissions.IsAuthenticatedOrReadOnly'],
+        'user': ['api.permissions.IsAuthenticatedOrReadOnly'],
 
     },
     'SERIALIZERS': {
